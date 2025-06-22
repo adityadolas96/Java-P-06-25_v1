3a683779f5a52d98e9068fcc5fc47ee4967515a6
adityadolas96 <joninjaxx96@gmail.com>
2025-06-22 18:41:00 +0530

    SC

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@AllArgsConstructor
+class Employee{
+    private int id;
+    private String name;
+    private double salary;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employee = Arrays.asList(new Employee(1,"aditya",30000),
+                new Employee(2,"avinash", 40000),
+                new Employee(3,"arvind",90000));
+
+        // Second Highest Salary
+        Optional<Double> secondHighestSalary = employee.stream()
+                .map(Employee::getSalary)
+                .distinct()
+                .sorted()
+                .skip(1)
+                .findFirst();
+
+        System.out.println(secondHighestSalary.get());
+    }
+}
